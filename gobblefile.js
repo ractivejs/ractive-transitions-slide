0da71e631d963f580720a76fad4be9c0733181d1
Rich Harris <richard.a.harris@gmail.com>
2015-04-27 09:20:31 -0400

    add minified version

--- gobblefile.js
+++ gobblefile.js
@@ -2,14 +2,19 @@ var gobble = require( 'gobble' );
 
 gobble.cwd( __dirname );
 
-module.exports = gobble( 'src' )
-.transform( 'babel', {
-	blacklist: [ 'es6.modules', 'useStrict' ],
-	sourceMap: false
-})
-.transform( 'esperanto-bundle', {
-	entry: 'ractive-transitions-slide',
-	type: 'umd',
-	name: 'Ractive.transitions.slide',
-	sourceMap: false
-});
+var lib = gobble( 'src' )
+	.transform( 'babel', {
+		blacklist: [ 'es6.modules', 'useStrict' ],
+		sourceMap: false
+	})
+	.transform( 'esperanto-bundle', {
+		entry: 'ractive-transitions-slide',
+		type: 'umd',
+		name: 'Ractive.transitions.slide',
+		sourceMap: false
+	});
+
+module.exports = gobble([
+	lib,
+	lib.transform( 'uglifyjs', { ext: '.min.js' })
+]);
